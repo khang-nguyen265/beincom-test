dc1f6f8925475b043428a9302652ad90c011247b
khang_nguyen <khang.d.d.nguyen@gmail.com>
2024-05-22 16:45:45 +0700

    update post type

--- src/lib/fetchPost.ts
+++ src/lib/fetchPost.ts
@@ -1,9 +1,5 @@
+import { Post } from "@/app/type";
 
-export interface Post {
-  id: number;
-  title: string;
-  body: string;
-}
 
 export async function fetchPost(id: number): Promise<Post> {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
