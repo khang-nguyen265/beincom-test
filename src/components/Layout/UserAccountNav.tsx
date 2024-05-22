a02396da9669991f22ddac2295959e5cfcc0fab5
khang_nguyen <khang.d.d.nguyen@gmail.com>
2024-05-22 18:06:22 +0700

    update sign-out flow

--- src/components/Layout/UserAccountNav.tsx
+++ src/components/Layout/UserAccountNav.tsx
@@ -23,7 +23,7 @@ const UserAccountNav = () => {
       }
       {
         isAuth && (
-          <UserButton afterSignOutUrl="/" />
+          <UserButton afterSignOutUrl="/sign-in" />
         )
       }
 
